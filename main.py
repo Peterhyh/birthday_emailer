ea61c9116774c5dc14d295910246d9a344c63661
Peter Huynh <peterhyh@yahoo.com>
2024-10-03 19:42:33 -0500

    first commit

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from classified import email_password
 #The email you want to use to send 
 my_email = "codingneatpete@gmail.com"
 
+receiving_email = "peterhyh@yahoo.com"
+
+my_message = "Hello"
+
 
 #connect to email provider smtp server with this object
 # For the class, .SMTP(), you must specify the location of email provider's SMTP server in the first parameter. For Gmail, it's smtp.gmail.com
@@ -15,3 +19,13 @@ connection.starttls()
 
 #logging in
 connection.login(user=my_email, password=email_password)
+
+#sending the mail
+connection.sendmail(from_addr=my_email, to_addrs=receiving_email, msg=my_message)
+
+#closing the connection after email has been sent
+connection.close()
+
+
+# must turn on 2-step verification for Gmail and create an app password
+
